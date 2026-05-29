ed539c3fd947bc55e593b0e9bd8e07f1e00befa3
Joel Jimenez <joel.jimenez@altia.com>
2026-05-29 16:32:12 +0200

    aaa

--- Demilingua-java-app/app/src/main/java/com/example/demilingua/DemilinguaApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.demilingua;
-
-import android.app.Application;
-import dagger.hilt.android.HiltAndroidApp;
-
-@HiltAndroidApp
-public class DemilinguaApplication extends Application {
-}
